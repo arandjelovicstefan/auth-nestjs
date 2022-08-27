c8780d0311cfde608f83d5c190548dde4fa5724b
Stefan Arandjelovic <arandjelovic.stefan@protonmail.ch>
2022-08-27 05:29:23 +0200

    feature reports

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { CreateReportDTO } from './reports-dtos/create-report.dto';
+import { ReportsService } from './reports.service';
 
-@Controller()
-export class ReportsController {}
+@Controller('reports')
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  createReport(@Body() body: CreateReportDTO) {
+    const report = this.reportsService.create(body);
+    return report;
+  }
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateReportDTO } from './reports-dtos/create-report.dto';
+import { Report } from './reports.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(Report) private reportsRepository: Repository<Report>,
+  ) {}
+
+  create(reportDTO: CreateReportDTO) {
+    const report = this.reportsRepository.create(reportDTO); // sa create pravimo novu instancu entiteta u bazi dok sa save cuvamo
+    return this.reportsRepository.save(report);
+  }
+}
